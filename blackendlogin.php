2e526e774ca695b76442fce81aacd1e981ca81fd
phorntip03 <oopontip7524@gmail.com>
2025-03-14 17:18:04 +0700

    ีup

--- blackendlogin.php
+++ blackendlogin.php
@@ -72,15 +72,14 @@
         <label for="floatingPassword">รหัสผ่าน</label>
       </div>
       <button class="btn btn-primary w-100 py-2" type="submit">เข้าสู่ระบบ</button>
-      <button id="cancelButton" class="btn btn-danger w-100 py-2 mt-2" type="button">ยกเลิก</button>
-      <p class="mt-5 mb-3 text-body-secondary text-center">© 2024 Phorntip, Sakchai</p>
+      <p class="mt-5 mb-3 text-body-secondary text-center">© 2025 คณะเกษตรศาสตร์และเทคโนโลยี มทร.อีสาน วข.สุรินทร์</p>
     </form>
   </main>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
           integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+Y1J3zn4=" crossorigin="anonymous"></script>
   <script>
     document.getElementById('cancelButton').addEventListener('click', function() {
-      window.location.href = 'p1.php'; // เปลี่ยน URL ตามที่ต้องการ
+      window.location.href = 'p1.php'; 
     });
   </script>
 </body>
