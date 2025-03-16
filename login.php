62abf5e29aaffd747b36ca14151bd76c8f318fe5
phorntip03 <oopontip7524@gmail.com>
2025-03-16 16:32:22 +0700

    i

--- login.php
+++ login.php
@@ -1,12 +1,12 @@
 <?php
 session_start();
-include 'connect.php'; // เชื่อมต่อฐานข้อมูล
+include 'connect.php'; 
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $conn->real_escape_string($_POST['username']);
     $password = $conn->real_escape_string($_POST['password']);
 
-    // ตรวจสอบการเชื่อมต่อฐานข้อมูล
+
     if ($conn) {
         // SQL คำสั่งในการดึงข้อมูลผู้ใช้
         $sql = "SELECT * FROM login WHERE username = '$username' AND password = '$password'";
@@ -14,15 +14,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         if ($result->num_rows > 0) {
             // ล็อคอินสำเร็จ
-            $_SESSION['username'] = $username; // เก็บชื่อผู้ใช้ใน session
-            header("Location: blackendhome.php"); // เปลี่ยนเส้นทางไปที่หน้า dashboard หรือหน้าหลักของผู้ใช้
+            $_SESSION['username'] = $username;
+            header("Location: blackendhome.php"); 
             exit();
         } else {
             // ล็อคอินไม่สำเร็จ
             echo "<script>alert('ชื่อผู้ใช้หรือรหัสผ่านไม่ถูกต้อง'); window.location.href='blackendlogin.php';</script>";
         }
 
-        // ปิดการเชื่อมต่อ
         $conn->close();
     } else {
         echo "Connection failed: " . $conn->connect_error;
