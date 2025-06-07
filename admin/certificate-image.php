d875d633a989caeaa5ee5dcf54d3f263484e6231
phorntip03 <oopontip7524@gmail.com>
2025-06-07 20:14:35 +0700

    ีup

--- admin/certificate-image.php
+++ admin/certificate-image.php
@@ -7,7 +7,7 @@ if (!isset($_GET['student_id'])) {
 
 $student_id = intval($_GET['student_id']);
 
-// ดึงข้อมูล
+// ดึงข้อมูลนักเรียน, คอร์ส และโมดูล
 $sql = "SELECT 
             s.name_st, 
             s.lastname_st, 
@@ -32,40 +32,28 @@ $student_name = $data['name_st'] . " " . $data['lastname_st'];
 $course_name = $data['name_th_course'];
 $module_name = $data['name_th_modulecourse'];
 
-<<<<<<< HEAD
-
-=======
 // สร้างภาพ
->>>>>>> 41b33e880676014d4837bb927cda69928271cac1
 $image_path = '../assets/certificate/c1.png';
+if (!file_exists($image_path)) {
+    die("Certificate image not found.");
+}
+
 header('Content-Type: image/png');
 $image = imagecreatefrompng($image_path);
 $black = imagecolorallocate($image, 0, 0, 0);
 
-<<<<<<< HEAD
-
-=======
-// โหลดฟอนต์ Sarabun จาก assets/fonts/
->>>>>>> 41b33e880676014d4837bb927cda69928271cac1
+// โหลดฟอนต์
 $font_path = '../assets/fonts/THSarabunNew.ttf';
 if (!file_exists($font_path)) {
     die("Font file not found.");
 }
 
-<<<<<<< HEAD
-
-=======
-// วาดข้อความลงภาพ (กำหนดขนาด และตำแหน่ง)
->>>>>>> 41b33e880676014d4837bb927cda69928271cac1
+// วาดข้อความลงบนภาพ
 imagettftext($image, 48, 0, 300, 340, $black, $font_path, $student_name);
 imagettftext($image, 36, 0, 300, 410, $black, $font_path, $course_name);
 imagettftext($image, 36, 0, 300, 480, $black, $font_path, $module_name);
 
-<<<<<<< HEAD
-
-=======
 // ส่งภาพออกไป
->>>>>>> 41b33e880676014d4837bb927cda69928271cac1
 imagepng($image);
 imagedestroy($image);
 ?>
